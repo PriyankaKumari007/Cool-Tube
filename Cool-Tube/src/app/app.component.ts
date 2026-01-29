a0a2129b7091c05f9e7c6dab80c9f8fcdf72606a
priyanka <priyankakumari6709@gmail.com>
2026-01-29 23:39:43 +0530

    api integrated

--- Cool-Tube/src/app/app.component.ts
+++ Cool-Tube/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { PexelsService } from './services/services/pexels.service';
+import { PixabayService } from './services/services/pixabay-services/pixabay.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Cool-Tube';
+  videos:any[] = [];
+ photos: any[] = [];
+  constructor(public pexelsService: PexelsService,
+    private pixabayService: PixabayService
+  ) { }
+
+  ngOnInit(): void {
+    this.pexelsService.getVideos().subscribe((response: any) => {
+      this.videos = response.videos;
+      console.log(this.videos);
+    });
+
+    //  this.pixabayService.getPhotos('nature').subscribe((data: any) => {
+    //   this.photos = data.hits; // 'hits' contains the array of photos
+    // });
+     this.loadAllPhotos('nature');
+  }
+    loadAllPhotos(query: string): void {
+    const totalPages = 3; // Fetch up to 3 pages (adjust as needed)
+    for (let page = 1; page <= totalPages; page++) {
+      this.pixabayService.getPhotos(query, 200, page).subscribe((data: any) => {
+        this.photos = [...this.photos, ...data.hits]; // Combine results
+      });
+    }
+  }
 }
