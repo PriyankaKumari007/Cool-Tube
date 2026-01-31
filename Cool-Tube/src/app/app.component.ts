12483283f48bdcb31c0c0c5d5694867e1556206a
priyanka <priyankakumari6709@gmail.com>
2026-01-31 17:20:50 +0530

    improved styles

--- Cool-Tube/src/app/app.component.ts
+++ Cool-Tube/src/app/app.component.ts
@@ -2,25 +2,25 @@ import { Component } from '@angular/core';
 import { PexelsService } from './services/services/pexels.service';
 import { PixabayService } from './services/services/pixabay-services/pixabay.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone:false
+  standalone: false,
 })
 export class AppComponent {
   title = 'Cool-Tube';
-  videos:any[] = [];
-photos: any[] = [];        // all fetched photos
-visiblePhotos: any[] = []; // shown in UI
+  videos: any[] = [];
+  photos: any[] = []; // all fetched photos
+  visiblePhotos: any[] = []; // shown in UI
 
-pageSize = 20;
-currentIndex = 0;
+  pageSize = 20;
+  currentIndex = 0;
 
-  constructor(public pexelsService: PexelsService,
-    private pixabayService: PixabayService
-  ) { }
+  constructor(
+    public pexelsService: PexelsService,
+    private pixabayService: PixabayService,
+  ) {}
 
   ngOnInit(): void {
     this.pexelsService.getVideos().subscribe((response: any) => {
@@ -28,42 +28,37 @@ currentIndex = 0;
       console.log(this.videos);
     });
 
-    //  this.pixabayService.getPhotos('nature').subscribe((data: any) => {
-    //   this.photos = data.hits; // 'hits' contains the array of photos
-    // });
-     this.loadAllPhotos('nature');
-     
+    this.loadAllPhotos('nature');
   }
-   loadAllPhotos(query: string) {
-  this.pixabayService.getPhotos(query, 200, 1).subscribe((data: any) => {
-    this.photos = data.hits;
-    this.loadMorePhotos(); // initial load
-  });
-}
 
-loadMorePhotos() {
-  const nextChunk = this.photos.slice(
-    this.currentIndex,
-    this.currentIndex + this.pageSize
-  );
-
-  this.visiblePhotos.push(...nextChunk);
-  this.currentIndex += this.pageSize;
-}
-
-
-onHorizontalScroll(event: Event) {
-  const element = event.target as HTMLElement;
-
-  const nearEnd =
-    element.scrollLeft + element.clientWidth >=
-    element.scrollWidth - 100;
-
-  if (nearEnd) {
-    this.loadMorePhotos();
+  
+  loadAllPhotos(query: string) {
+    this.pixabayService.getPhotos(query, 200, 1).subscribe((data: any) => {
+      this.photos = data.hits;
+      this.loadMorePhotos(); // initial load
+    });
   }
-}
 
+  loadMorePhotos() {
+    const nextChunk = this.photos.slice(
+      this.currentIndex,
+      this.currentIndex + this.pageSize,
+    );
+
+    this.visiblePhotos.push(...nextChunk);
+    this.currentIndex += this.pageSize;
+  }
+
+  onHorizontalScroll(event: Event) {
+    const element = event.target as HTMLElement;
+
+    const nearEnd =
+      element.scrollLeft + element.clientWidth >= element.scrollWidth - 100;
+
+    if (nearEnd) {
+      this.loadMorePhotos();
+    }
+  }
 
   /* Added methods to handle carousel scrolling */
   scrollLeft() {
@@ -79,6 +74,9 @@ onHorizontalScroll(event: Event) {
   /* Added method to handle mouse wheel scrolling for the carousel */
   onCarouselScroll(event: WheelEvent) {
     const container = document.querySelector('.photo-container') as HTMLElement;
-    container.scrollBy({ left: event.deltaY < 0 ? -300 : 300, behavior: 'smooth' });
+    container.scrollBy({
+      left: event.deltaY < 0 ? -300 : 300,
+      behavior: 'smooth',
+    });
   }
 }
